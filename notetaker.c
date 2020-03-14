8ab7e635f815473aaa0d6a8c6aefa944f61401ae
mce9 <dwing@noncosi.com>
2020-03-14 13:16:57 +0100

    Add hacking.h, notetaker.c

---
+++ notetaker.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fnctl.h>
+#include <sys/stat.h>
+#include "hacking.h"
+
+void usage(char *prog_name, char *filename) {
+    printf("Usage: %s <data to add to %s>\n", prog_name, filename);
+    exit(0);
+}
+
+void fatal(char *);                         // A function for fatal errors
+void *ec_malloc(unsigned int);              // An error-checked malloc() wrapper
+
+int main(int argc, char *argv[]) {
+    int userid, fd;                         // File descriptor
+    char *buffer, *datafile
+
+    buffer = (char *) ec_malloc(100);
+    datafile = (char *) ec_malloc(200);
+    strcpy(datafile, "/var/notes");
+
+    if(argc < 2)
+        usage(argv[0], datafile);
+    
+    strcpy(buffer, argv[1]);
+
+    printf("[DEBUG] buffer   @ %p: \'%s\'\n", buffer, buffer);
+    printf("[DEBUG] datafile @ %p: \'%s\'\n", datafile, datafile);
+
+    // Opening the file
+    fd = open(datafile, O_WRONLY|O_CREAT|O_APPEND, S_IRUSR|S_IWUSR);
+    if(fd == -1)
+        fatal("in main() while opening file");
+    printf("[DEBUG] file descriptor is %d\n", fd);
+    userid = getuid();                      // get the real user ID
+
+    // Writing data
+    if(write(fd, &userid, 4) == -1)         // Write user ID before note data
+        fatal("in main() while writing userid to file");
+    write(fd, "\n", 1);                     // terminate line
+
+    if(write(fd, buffer, strlen(buffer)) == -1) // write note
+        fatal("in main() while writing buffer to file");
+    write(fd, "\n", 1);                     // terminate line
+
+    // Closing file
+    if(close(fd) == -1)
+        fatal("in main() while closing file");
+
+    printf("Note has been saved.\n");
+    free(buffer);
+    free(datafile);
+}
